38dcaafcacc9b6db175561e44209ba6709fee31f
vitorvenacio <vitorvenacio@gmail.com>
2021-08-03 14:15:39 -0300

    Finalizada

--- calculadoraangular/src/app/app.module.ts
+++ calculadoraangular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalculadoraModule } from './calculadora';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculadoraModule } from './calculadora';
   ],
   imports: [
     BrowserModule,
-    CalculadoraModule
+    CalculadoraModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- calculadoraangular/src/app/calculadora/components/calculadora.component.ts
+++ calculadoraangular/src/app/calculadora/components/calculadora.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { convertTypeAcquisitionFromJson } from 'typescript';
 import { CalculadoraService } from '../services';
 @Component({
   selector: 'app-calculadora',
@@ -7,9 +8,85 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
+
+  constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
+    this.limpar();
   }
+
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if (numAtual === '0' || numAtual === null) {
+      numAtual = '';
+    }
+
+    if (numConcat === '.' && numAtual === '') {
+      return '0.';
+    }
+
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    }
+    return numAtual + numConcat;
+  }
+
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    if (this.numero2 !== null) {
+
+      this.resultado = this.calculadoraService.calcular(parseFloat(this.numero1), parseFloat(this.numero2),this.operacao);
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+    
+  }
+  calcular(): void {
+  	if (this.numero2 === null) {
+  		return;
+  	}
+
+  	this.resultado = this.calculadoraService.calcular(
+  		parseFloat(this.numero1),
+  		parseFloat(this.numero2),
+  		this.operacao);
+  }
+
+  get display(): string {
+  	if (this.resultado !== null) {
+  		return this.resultado.toString();
+  	}
+  	if (this.numero2 !== null) {
+  		return this.numero2;
+  	}
+  	return this.numero1;
+  }
+
+
+
 
 }
